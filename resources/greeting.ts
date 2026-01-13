fc1ec5105e2a3999f4771ab97cda3631e68711de
Dawson Toth <dawson@harperdb.io>
2026-01-13 16:55:38 -0500

    feat: Add strong type examples for tables and resources

--- resources/greeting.ts
+++ resources/greeting.ts
@@ -1,9 +1,29 @@
-import {Resource} from 'harperdb';
+import { type RecordObject, type RequestTargetOrId, Resource } from 'harperdb';
 
-export class greeting extends Resource {
+interface GreetingRecord {
+	greeting: string;
+}
+
+export class Greeting extends Resource<GreetingRecord> {
 	static loadAsInstance = false;
 
-	async get() {
-		return { greeting: 'Hello, world!' };
+	async post(target: RequestTargetOrId, newRecord: Partial<GreetingRecord & RecordObject>): Promise<GreetingRecord> {
+		return { greeting: 'Greetings, post!' };
+	}
+
+	async get(target?: RequestTargetOrId): Promise<GreetingRecord> {
+		return { greeting: 'Greetings, get!' };
+	}
+
+	async put(target: RequestTargetOrId, record: GreetingRecord & RecordObject): Promise<GreetingRecord> {
+		return { greeting: 'Greetings, put!' };
+	}
+
+	async patch(target: RequestTargetOrId, record: Partial<GreetingRecord & RecordObject>): Promise<GreetingRecord> {
+		return { greeting: 'Greetings, patch!' };
+	}
+
+	async delete(target: RequestTargetOrId): Promise<boolean> {
+		return true
 	}
 }
